a5e4d2e3f50cc5a1723a15ef4d546e706ce7acad
teabao <45088209+teabao@users.noreply.github.com>
2021-05-20 22:12:16 +0800

    cv

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ import sys
 import tempfile
 from argparse import ArgumentParser
 
-import cv2 as cv
+import cv2
 import numpy as np
 from flask import Flask, abort, request, send_from_directory
 from linebot import LineBotApi, WebhookHandler
